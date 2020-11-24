f26f779066eb21ef981c4bd49f53c1b943093b9e
lboz <l.bozzi@betclicgroup.com>
2020-11-24 09:29:59 +0100

    Create new component for table dark Remove encapsulation for table light / dark

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,14 +6,15 @@ import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 @NgModule({
-  declarations: [BtnComponent, TableLightComponent, TotalPipe, StateDirective],
+  declarations: [BtnComponent, TableLightComponent, TotalPipe, StateDirective, TableDarkComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  exports: [TableLightComponent,BtnComponent,TotalPipe, StateDirective]
+  exports: [TableLightComponent,BtnComponent,TotalPipe, StateDirective, TableDarkComponent]
 })
 export class SharedModule { }
